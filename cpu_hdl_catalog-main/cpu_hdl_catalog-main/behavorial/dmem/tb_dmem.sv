a44e69285977fc11aa8b7cc6c02183b364603a96
Shayna Levin <shayna.levin@cooper.edu>
2026-04-30 02:11:23 -0400

    Changed up the assignments

--- cpu_hdl_catalog-main/cpu_hdl_catalog-main/behavorial/dmem/tb_dmem.sv
+++ cpu_hdl_catalog-main/cpu_hdl_catalog-main/behavorial/dmem/tb_dmem.sv
@@ -33,23 +33,31 @@ module tb_dmem;
     end
 
     initial begin
-        #10 clock_enable <= 1;
-        #20 writedata = #(n)'hFFFFFFFF;
-        #20 dmem_addr <= #(r)'b000000;
-        #20 write_enable <= 1;
-        #20 write_enable <= 0;
-        #20 dmem_addr <= #(r)'b000001;
-        #20 writedata = #(n)'h0000FFFF;
-        #20 write_enable <= 1;
-        #20 write_enable <= 0;
-        #20 dmem_addr <= #(r)'b000010;
-        #20 writedata = #(n)'h00000000;
-        #20 write_enable <= 1;
-        #20 write_enable <= 0;
-        #20 $finish;
+        clock_enable 0;
+        write_enable = 0;
+        dmem_addr = 0;
+        #20 writedata = 0;
+        #10 clock_enable = 1;
+        
+        // Write FFFFFFFF to Address 0
+        #20 dmem_addr = 32'h00000000; writedata = 32'hFFFFFFFF;
+        #20 write_enable = 1;
+        #20 write_enable = 0;
+        
+        // Write 0000FFFF to Address 4
+        #20 dmem_addr = 32'h00000004; writedata = 32'h0000FFFF;
+        #20 write_enable = 1;
+        #20 write_enable = 0;
+        
+        // Write 12345678 to Address 8
+        #20 dmem_addr = 32'h00000008; writedata = 32'h12345678;
+        #20 write_enable = 1;
+        #20 write_enable = 0;
+        
+        #40 $finish;
     end
 
-   dmem uut(
+   dmem #(n, r)uut(
         .clk(clk),
         .write_enable(write_enable),
         .addr(dmem_addr),
